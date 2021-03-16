2744b8cb942ef2106545e01c2ce64738929306e0
slink007 <slink_1@cox.net>
2021-03-16 14:51:54 -0400

    Added a button to control when LED lights up

--- morse_code/morse_code.ino
+++ morse_code/morse_code.ino
@@ -1,6 +1,6 @@
-// A slightly more complicated "Hello World" program.
-// Instead of blinking an LED on and off this will do so with 
-// a Morse Code pattern.  S-O-S
+// Add a button to the morse code setup.
+// When the button is pressed the code is sent.  When the 
+// button is not pressed the LED remains off.
 
 // 'dit' is 1 unit long
 // 'dah' is 3 units long
@@ -8,6 +8,7 @@
 //  pause between words is 7 units
 
 #define LED 8
+#define BUTTON 10
 #define UNIT 250
 #define DIT UNIT
 #define DAH (UNIT * 3)
@@ -16,49 +17,52 @@
 
 void setup() {
   pinMode(LED, OUTPUT);    // LED on pin 8
+  pinMode(BUTTON, INPUT);  // button on pin 2
 }
 
 void loop() {
-  // Show 'S'
-  digitalWrite(LED, HIGH);
-  delay(DIT);
-  digitalWrite(LED, LOW);
-  delay(UNIT);
-  digitalWrite(LED, HIGH);
-  delay(DIT);
-  digitalWrite(LED, LOW);
-  delay(UNIT);
-  digitalWrite(LED, HIGH);
-  delay(DIT);
-  digitalWrite(LED, LOW);
-  delay(NEXTLETTER);
-
-  // Show 'O'
-  digitalWrite(LED, HIGH);
-  delay(DAH);
-  digitalWrite(LED, LOW);
-  delay(UNIT);
-  digitalWrite(LED, HIGH);
-  delay(DAH);
-  digitalWrite(LED, LOW);
-  delay(UNIT);
-  digitalWrite(LED, HIGH);
-  delay(DAH);
-  digitalWrite(LED, LOW);
-  delay(NEXTLETTER);
-
-  // Show 'S'
-  digitalWrite(LED, HIGH);
-  delay(DIT);
-  digitalWrite(LED, LOW);
-  delay(UNIT);
-  digitalWrite(LED, HIGH);
-  delay(DIT);
-  digitalWrite(LED, LOW);
-  delay(UNIT);
-  digitalWrite(LED, HIGH);
-  delay(DIT);
-  digitalWrite(LED, LOW);
+  int buttonState = digitalRead(BUTTON);
+  if (buttonState == HIGH)
+  {
+    // Show 'S'
+    digitalWrite(LED, HIGH);
+    delay(DIT);
+    digitalWrite(LED, LOW);
+    delay(UNIT);
+    digitalWrite(LED, HIGH);
+    delay(DIT);
+    digitalWrite(LED, LOW);
+    delay(UNIT);
+    digitalWrite(LED, HIGH);
+    delay(DIT);
+    digitalWrite(LED, LOW);
+    delay(NEXTLETTER);
   
-  delay(NEXTWORD);
+    // Show 'O'
+    digitalWrite(LED, HIGH);
+    delay(DAH);
+    digitalWrite(LED, LOW);
+    delay(UNIT);
+    digitalWrite(LED, HIGH);
+    delay(DAH);
+    digitalWrite(LED, LOW);
+    delay(UNIT);
+    digitalWrite(LED, HIGH);
+    delay(DAH);
+    digitalWrite(LED, LOW);
+    delay(NEXTLETTER);
+  
+    // Show 'S'
+    digitalWrite(LED, HIGH);
+    delay(DIT);
+    digitalWrite(LED, LOW);
+    delay(UNIT);
+    digitalWrite(LED, HIGH);
+    delay(DIT);
+    digitalWrite(LED, LOW);
+    delay(UNIT);
+    digitalWrite(LED, HIGH);
+    delay(DIT);
+    digitalWrite(LED, LOW);
+  }
 }
